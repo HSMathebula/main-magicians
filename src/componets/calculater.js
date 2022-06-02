acbefe351be1f0da5feb9f133c12e09b4cd45387
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-06-02 11:43:21 +0200

    Adding props and state to calculator.js

---
+++ src/componets/calculater.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './calculator.css';
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+}
