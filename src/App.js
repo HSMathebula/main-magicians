f8b64b820fd11dc15a13e0f7858a95bcdd503a37
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-06-09 09:16:54 +0200

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Quote from './components/quote';
 const App = () => (
   <>
     <Header />
-    
+
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/calculator" element={<Calculator />} />
