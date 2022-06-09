eed532e6bd0034842b7b244d91157dc8bbf7cfec
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-06-09 09:02:43 +0200

    Using router to add pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/home';
 import './index.css';
 import './App.css';
+
 import Calculator from './components/calculator';
 import Header from './components/header';
 import Quote from './components/quote';
@@ -10,6 +11,7 @@ import Quote from './components/quote';
 const App = () => (
   <>
     <Header />
+    
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/calculator" element={<Calculator />} />
